c0b83e57579e4a6e5fe5ad0224308aeb4cba3902
y4ntagonist <150617013+y4ntagonist@users.noreply.github.com>
2023-11-13 03:16:41 +0800

    QuizGame.java

--- QuizGame.java
+++ QuizGame.java
@@ -5,10 +5,10 @@ public class QuizGame {
         Scanner scanner = new Scanner(System.in);
         List<Quiz> quizzes = new ArrayList<>();
 
-         while (true) {
-            System.out.println("1. Create a Quiz"); 
+        while (true) {
+            System.out.println("1. Create a Quiz");
             System.out.println("2. Take a Quiz");
-            System.out.println("3. View a Quiz"); 
+            System.out.println("3. View a Quiz");
             System.out.println("4. List of Quizzes");
             System.out.println("5. Exit");
             System.out.println("Enter a command:");
@@ -28,6 +28,8 @@ public class QuizGame {
                 System.out.println("Invalid command.");
             }
         }
+
+        scanner.close();
     }
 
     static void createQuiz(Scanner scanner, List<Quiz> quizzes) {
@@ -35,12 +37,13 @@ public class QuizGame {
         String quizName = scanner.nextLine();
         Quiz quiz = new Quiz(quizName);
 
-        while (true) {
-            System.out.print("Enter a question or 'done' to finish: ");
+        System.out.print("Enter the number of questions for the quiz: ");
+        int numberOfQuestions = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
+
+        for (int i = 0; i < numberOfQuestions; i++) {
+            System.out.print("Enter question " + (i + 1) + ": ");
             String question = scanner.nextLine();
-            if (question.equalsIgnoreCase("done")) {
-                break;
-            }
 
             List<String> choices = new ArrayList<>();
             char choiceChar = 'A';
@@ -75,3 +78,77 @@ public class QuizGame {
         for (int i = 0; i < quizzes.size(); i++) {
             System.out.println((i + 1) + ". " + quizzes.get(i).getName());
         }
+
+        int quizNumber = readInt(scanner, "Enter the number of the quiz you want to take: ", 1, quizzes.size());
+        Quiz selectedQuiz = quizzes.get(quizNumber - 1);
+        int score = 0;
+
+        for (int i = 0; i < selectedQuiz.getQuestionCount(); i++) {
+            Question currentQuestion = selectedQuiz.getQuestion(i);
+
+            System.out.println("Q" + (i + 1) + ": " + currentQuestion.getQuestion());
+
+            char choiceChar = 'A';
+
+            for (String choice : currentQuestion.getChoices()) {
+                System.out.println(choiceChar + ": " + choice);
+                choiceChar++;
+            }
+
+            System.out.print("Your choice (A, B, C, ...): ");
+            String userAnswer = scanner.nextLine().toUpperCase();
+
+            if (userAnswer.equals(currentQuestion.getCorrectChoice())) {
+                System.out.println("Correct!");
+                score++;
+            } else {
+                System.out.println("Incorrect. The correct answer is: " + currentQuestion.getCorrectChoice());
+            }
+        }
+
+        System.out.println("Quiz completed. Your score: " + score + " out of " + selectedQuiz.getQuestionCount());
+    }
+
+    static void viewQuiz(Scanner scanner, List<Quiz> quizzes) {
+        if (quizzes.isEmpty()) {
+            System.out.println("No quizzes available. Create a quiz first.");
+            return;
+        }
+
+        System.out.println("Available Quizzes:");
+        for (int i = 0; i < quizzes.size(); i++) {
+            System.out.println((i + 1) + ". " + quizzes.get(i).getName());
+        }
+
+        int quizNumber = readInt(scanner, "Enter the number of the quiz you want to view: ", 1, quizzes.size());
+        Quiz selectedQuiz = quizzes.get(quizNumber - 1);
+
+        System.out.println("Quiz: " + selectedQuiz.getName());
+
+        for (int i = 0; i < selectedQuiz.getQuestionCount(); i++) {
+            Question currentQuestion = selectedQuiz.getQuestion(i);
+
+            System.out.println("Q" + (i + 1) + ": " + currentQuestion.getQuestion());
+
+            char choiceChar = 'A';
+
+            for (String choice : currentQuestion.getChoices()) {
+                System.out.println(choiceChar + ": " + choice);
+                choiceChar++;
+            }
+
+            System.out.println("Correct Answer: " + currentQuestion.getCorrectChoice());
+        }
+    }
+
+    static void listQuizzes(List<Quiz> quizzes) {
+        if (quizzes.isEmpty()) {
+            System.out.println("No quizzes available. Create a quiz first.");
+        } else {
+            System.out.println("Available Quizzes:");
+            for (int i = 0; i < quizzes.size(); i++) {
+                System.out.println((i + 1) + ". " + quizzes.get(i).getName());
+            }
+        }
+    }
+}
